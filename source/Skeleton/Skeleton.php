05a5129649564ca59dbed400e648086a5fd4d065
Alexis von Glasow <vonglasow@gmail.com>
2014-08-29 17:40:06 +0200

    Add unit tests examples
    Provider
    Mock PHP Function
    Never used new in tests methods

--- source/Skeleton/Skeleton.php
+++ source/Skeleton/Skeleton.php
@@ -4,4 +4,24 @@ namespace Skeleton;
 
 class Skeleton
 {
+    public function getClass()
+    {
+        return (string) $this;
+    }
+
+    public function __toString()
+    {
+        return __CLASS__;
+    }
+
+    public function countArray()
+    {
+        return count(array(1, 2, 3, 4));
+    }
+
+    public function conditional($isExpired, $countInProgress, $countToCome)
+    {
+        return !(($isExpired && 0 <= ($countInProgress + $countToCome))
+                || (0 <= ($countInProgress + $countToCome) - 1));
+    }
 }
--- tests/units/Skeleton.php
+++ tests/units/Skeleton.php
@@ -6,11 +6,78 @@ use mageekguy\atoum;
 
 //require __DIR__ . '/../../vendor/autoload.php';
 
+/**
+ * To ignore class choose 'on'
+ * @ignore on/off
+ */
 class Skeleton extends atoum\test
 {
     public function testClass()
     {
-        $this->object(new \Skeleton\Skeleton)->isInstanceOf('\Skeleton\Skeleton')
+        $this->assert('Assert if object is good instance')
+            // No new needed can use $this->newTestedInstance(param1, param2, …)
+            ->object($this->newTestedInstance)->isInstanceOf('\Skeleton\Skeleton')
+            ->assert('Assert if object return good string')
+            // So as for getting object $this->testedInstance
+            ->string($this->testedInstance->getClass())->isEqualTo('Skeleton\Skeleton')
+        ;
+    }
+
+    /**
+     * To ignore test choose 'on'
+     * @ignore on/off
+     */
+    public function testCountArray()
+    {
+        $this->assert('Assert if we can mock count PHP function')
+            ->object($this->newTestedInstance)->isInstanceOf('\Skeleton\Skeleton')
+            // Mock PHP Function
+            ->if($this->function->count = 2)
+            ->integer($this->testedInstance->countArray())->isEqualTo(2)
+        ;
+    }
+
+
+    public function refactoFalse()
+    {
+        return array(
+            array(true, 0, 0),
+            array(false, 0, 5),
+            array(false, 5, 0),
+            array(true, 5, 5),
+            array(false, -5, 6),
+            array(true, 6, -5),
+        );
+    }
+
+    /**
+     * @dataProvider refactoFalse
+     */
+    public function testRefactoIfFalse($isExpired, $countInProgress, $countToCome)
+    {
+        $this
+            ->object($this->newTestedInstance)->isInstanceOf('\Skeleton\Skeleton')
+            ->boolean($this->testedInstance->conditional($isExpired, $countInProgress, $countToCome))->isFalse()
+        ;
+    }
+
+    public function refactoTrue()
+    {
+        return array(
+            array(true, -1, 0),
+            array(false, 0, 0),
+            array(true, -6, 5),
+        );
+    }
+
+    /**
+     * @dataProvider refactoTrue
+     */
+    public function testRefactoIfTrue($isExpired, $countInProgress, $countToCome)
+    {
+        $this
+            ->object($this->newTestedInstance)->isInstanceOf('\Skeleton\Skeleton')
+            ->boolean($this->testedInstance->conditional($isExpired, $countInProgress, $countToCome))->isTrue()
         ;
     }
 }
