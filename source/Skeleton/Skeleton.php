702caf344bfec0c9ed872ddb143c5a1a131c9259
Walid Dridi <walid.dridi@inovia48>
2015-01-23 11:35:34 +0100

    Add assert exception example

--- source/Skeleton/Skeleton.php
+++ source/Skeleton/Skeleton.php
@@ -29,4 +29,13 @@ class Skeleton
         return !(($isExpired && 0 <= ($countInProgress + $countToCome))
                 || (0 <= ($countInProgress + $countToCome) - 1));
     }
+
+    public function implodeArray($array)
+    {
+        if (!is_array($array)) {
+            throw new \Exception('Must be an array');
+        }
+
+        return implode(',', $array);
+    }
 }
